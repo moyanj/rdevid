500e71350f2a6cc1ad7c111697da81a87817627a
moyan <1561515308@qq.com>
2025-05-30 17:18:10 +0800

    build(c): 更新 C 绑定构建配置
    将生成的头文件输出到 target 目录

--- bindings/c/build.rs
+++ bindings/c/build.rs
@@ -1,11 +1,16 @@
 extern crate cbindgen;
 
 use std::env;
+use std::path::Path;
 
 fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let crate_dir = env::var("CARGO_WORKSPACE_DIR")
+        .or_else(|_| env::var("CARGO_MANIFEST_DIR"))
+        .unwrap();
+    let profile = env::var("PROFILE").unwrap(); // debug/release
+    let target_dir = Path::new(&crate_dir).join("../../target").join(&profile);
 
     cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings")
-        .write_to_file("rdevid.h");
+        .write_to_file(target_dir.join("rdevid.h"));
 }
